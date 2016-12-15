93aae4fb65ae7a8b37e48a583d7d52e12ff9d653
Kenneth <lee_zhunhoe@hotmail.com>
2016-12-15 10:52:00 +0800

    Add project files.

---
+++ ddac/Migrations/201612071448103_init.cs
@@ -0,0 +1,16 @@
+namespace ddac.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
